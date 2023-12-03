3b6b3b1b9cf562b507e9577933d3456897a78a80
Zhenya Sinyakin <evgeny.siniakin@yandex.ru>
2023-12-03 13:57:30 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,60 @@
+package org.example;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+    //    Инфо о сотрудниках
+    public static void main(String[] args) {
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> list = parseCSV(columnMapping, fileName);
+    }
+
+    //    Далее получите список сотрудников, вызвав метод parseCSV():
+    private static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        try (CSVReader reader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping(columnMapping);
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(reader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            List<Employee> list2 = csv.parse();
+            list2.forEach(System.out::println);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    //    Полученный список преобразуйте в строчку в формате JSON
+    private static List<Employee> jsonToList(String json) {
+        List<Employee> list3 = new ArrayList<>();
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        try {
+            Object obj = new JSONParser().parse(json);
+            JSONArray array = (JSONArray) obj;
+            for (Object a : array) {
+                list3.add(gson.fromJson(a.toString(), Employee.class));
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return list3;
+    }
+}
